1f3ae71146cd809c2b7c044e0382160833526590
Evolusin <98921577+Evolusin@users.noreply.github.com>
2022-02-11 20:14:59 +0100

    Send and close ticket

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import base64
 
 url = 'https://hd.infocomp.pl/api/Authorization'
 url2 = 'https://hd.infocomp.pl/api/ticket'
+url3 = 'https://hd.infocomp.pl/api/UpdateTicket'
 params = {
     'categoryId':36,
     'body':'sample',
     'subject':'Test API',
     'priorityId':0
 }
-
+# params2 = {
+#     'id':113982,
+#     'statusId':3
+# }
+list_tickets = []
 
 loginy = {
     'username': 'bartlomiej.bruzdowski@infocomp.pl',
@@ -28,7 +33,31 @@ def get_header(data):
         ).decode("utf8")
     }
 print(get_header(loginy))
-connector = requests.post(url2, headers=get_header(loginy), params=params)
-print(connector.status_code)
-r=connector.text
-print(r)
+
+#send ticket
+def send_ticket(url,params):
+    connector = requests.post(url, headers=get_header(loginy), params=params)
+    print(connector.status_code)
+    r=connector.text
+    list_tickets.append(r)
+
+
+send_ticket(url2,params)
+list_tickets = list(map(int,list_tickets))
+print(list_tickets)
+
+
+
+def close_ticket(url,params):
+    connector = requests.post(url, headers=get_header(loginy), params=params)
+    print(connector.status_code)
+    t = connector.text
+
+for i in range(len(list_tickets)):
+    params2={
+        'id':list_tickets[i],
+        'statusId':3,
+        'assignedUserId':734
+    }
+    print(params2)
+    close_ticket(url3,params2)
